0e89aa951b14979b12a1698832c3633522933aae
VictoriaEtienne <victoria.etienne@outlook.fr>
2024-03-05 14:53:19 +0100

    geocoding

---
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+export default class extends Controller {
+  static values = { apiKey: String }
+
+  static targets = ["address"]
+
+  connect() {
+    this.geocoder = new MapboxGeocoder({
+      accessToken: this.apiKeyValue,
+      types: "country,region,place,postcode,locality,neighborhood,address"
+    })
+
+    // Ajouter les événements "result" et "clear" au geocoder
+    this.geocoder.on("result", event => this.setInputValue(event))
+    this.geocoder.on("clear", () => this.clearInputValue())
+
+    this.geocoder.addTo(this.element)
+  }
+
+  disconnect() {
+    this.geocoder.off("result")
+    this.geocoder.off("clear")
+    this.geocoder.onRemove()
+  }
+
+  setInputValue(event) {
+    // Mise à jour de la valeur de l'élément cible avec le résultat du géocode
+    this.addressTarget.value = event.result["place_name"]
+  }
+
+  clearInputValue() {
+    // Effacer la valeur de l'élément cible lors de l'événement "clear"
+    this.addressTarget.value = ""
+  }
+}
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ApplicationRecord
   has_many :shop_alternatives
   has_many :product_alternatives, through: :shop_alternatives
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
