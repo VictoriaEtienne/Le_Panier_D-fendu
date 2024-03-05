3d7baf6d291215a411c9cc309b002b19e1a30e7a
Anne-Alois ROGER <annealois@gmail.com>
2024-03-05 11:31:00 +0100

    status

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+CSV_PRODUCTS = File.join(Rails.root, "db", "seeds", "agribalyse_synthese_v1.csv")
+product = 
+
+csv_file   = File.join(__dir__, 'recipes.csv')
+cookbook   = Cookbook.new(csv_file)
