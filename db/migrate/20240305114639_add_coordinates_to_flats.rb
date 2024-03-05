b7e3c12ca03bec67bd33ed6e23ff2100b2f292f1
VictoriaEtienne <victoria.etienne@outlook.fr>
2024-03-05 15:05:07 +0100

    flatstoshops

--- db/migrate/20240305114639_add_coordinates_to_flats.rb
+++ db/migrate/20240305114639_add_coordinates_to_flats.rb
@@ -1,6 +1,6 @@
 class AddCoordinatesToFlats < ActiveRecord::Migration[7.1]
   def change
-    add_column :flats, :latitude, :float
-    add_column :flats, :longitude, :float
+    add_column :shops, :latitude, :float
+    add_column :shops, :longitude, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_04_162107) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_114639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_162107) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
