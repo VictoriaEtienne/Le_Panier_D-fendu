8d74b1767cc24605985024aebccf57b2eaed9915
sofiane tabbi anneni <s.tabbianneni@gmail.com>
2024-03-07 12:02:04 +0100

    Move all_shops routes to shops index action

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
   resources :histories, except: [:edit, :update]
 
   resources :product_alternatives, only: :show do
@@ -16,9 +17,7 @@ Rails.application.routes.draw do
     resources :shops, only: :index
   end
 
-  resources :shops, only: [:show] do
-    get :all_shops, on: :collection, as: :all
-  end
+  resources :shops, only: [:index, :show]
 
   get "home_new", to: "pages#home_new"
   get "dashboard", to: "pages#dashboard", as: :dashboard
