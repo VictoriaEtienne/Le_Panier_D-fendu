2eea005d6349cc66cbbb9b9b58f9ddebb17ae8a5
sofiane tabbi anneni <s.tabbianneni@gmail.com>
2024-03-07 12:03:09 +0100

    Nest shops resources within histories

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :histories, except: [:edit, :update]
+  resources :histories, except: [:edit, :update] do
+    resources :shops, only: [:index, :show]
+  end
 
   resources :product_alternatives, only: :show do
     collection do
@@ -17,6 +19,7 @@ Rails.application.routes.draw do
     resources :shops, only: :index
   end
 
+  # TODO: a remplacer quand le resources shops nesté dans histories sera correctement connecté
   resources :shops, only: [:index, :show]
 
   get "home_new", to: "pages#home_new"
